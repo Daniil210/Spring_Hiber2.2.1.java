9ec6bb8e21ba531ba1a5b2c119654c1ad33cc8a7
Daniil210 <den.iv0008@gmail.com>
2021-07-19 20:26:03 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,65 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User vasya = new User("Vasya", "Vasechkin", "vasechkin@mail.io");
+      User petya = new User("Petya", "Sidorov", "sidorov@mail.io");
+      User olga = new User("Olga", "Petrova", "petrova@mail.io");
+      User sveta = new User("Svetlana", "Ivanova", "ivanova@mail.io");
+
+      Car volvo = new Car("Volvo", 9);
+      Car bmw = new Car("BMW", 325);
+      Car suzuki = new Car("Sisuki", 4);
+      Car lada = new Car("Ladaa", 21014);
+
+      vasya.setCar(volvo);
+      volvo.setUser(vasya);
+      userService.add(vasya);
+
+      petya.setCar(bmw);
+      bmw.setUser(petya);
+      userService.add(petya);
+
+      olga.setCar(suzuki);
+      suzuki.setUser(olga);
+      userService.add(olga);
+
+      sveta.setCar(lada);
+      lada.setUser(sveta);
+      userService.add(sveta);
+
+      // пользователи с машинами
+      for (User user : userService.listUsers()) {
+
+         System.out.println(user + " " + user.getCar());
+      }
+
+      System.out.println();
+      // достать юзера, владеющего машиной по ее модели и серии
+      System.out.println(userService.getUserByCar("BMW", 325));
+
+      System.out.println();
+      // нет такого юзера с такой машиной
+      try {
+         User notFoundUser = userService.getUserByCar("GAZ", 4211);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
+      }
+
+      context.close();
+   }
+}
